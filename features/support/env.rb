f213128a4004b72c524aa0a21c5d3b3daf46f87e
Andy Waite <github.aw@andywaite.com>
2012-06-05 12:01:46 +0100

    Grab app name from the Rakefile

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,4 +3,6 @@ require 'frank-cucumber'
 # UIQuery is deprecated. Please use the shelley selector engine. 
 Frank::Cucumber::FrankHelper.use_shelley_from_now_on
 
-APP_BUNDLE_PATH = File.expand_path('build/iPhoneSimulator-5.1-Development/Your App Name.app')
+rakefile_contents = File.open(File.expand_path('Rakefile')).read
+app_name = rakefile_contents.match(/app\.name = '(.*)'/)[1]
+APP_BUNDLE_PATH = File.expand_path("build/iPhoneSimulator-5.1-Development/#{app_name}.app")
