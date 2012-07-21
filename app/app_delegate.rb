da858d033c5db02b4c8cfb370a30be15f22b01ba
Andy Waite <github.aw@andywaite.com>
2012-07-21 13:10:51 +0100

    Don't launch app if running tests

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,6 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    return true if RUBYMOTION_ENV == 'test'
     home_view_controller = HomeViewController.alloc.init
     navigation_controller = UINavigationController.alloc.initWithRootViewController(home_view_controller)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
